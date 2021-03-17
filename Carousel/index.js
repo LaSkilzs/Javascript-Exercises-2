66984e8ac6598d3d377734bd9d969ac73c5cb170
Operations branmanagement <7775577-operations20@users.noreply.gitlab.com>
2021-03-17 16:16:22 -0400

    Reviewed and Updated code

---
+++ Carousel/index.js
@@ -0,0 +1,21 @@
+// Function Show
+
+function showSlide(slideIndex) {
+    const slides = document.getElementsByClassName('carousel');
+    // complete the following function 
+}
+
+// Function  Next
+function previousSlide() {
+    // complete the following function
+}
+
+
+// Function Prev
+function nextSlide() {
+    // complete the following function
+}
+
+showSlide();
+    
+   
