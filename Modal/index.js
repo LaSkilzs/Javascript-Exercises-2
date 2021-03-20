596fb01d7662749d12bb8c5804584a8232c75fae
Gabriel Lebec <glebec@gmail.com>
2021-03-20 02:10:58 -0400

    feat: normalize formatting via prettier

--- Modal/index.js
+++ Modal/index.js
@@ -1,21 +1,19 @@
-
-// Create a new function
+// Create a new Alert
 let Alert = new CustomAlert();
 
+// create functions for Alert
+function CustomAlert() {
+    // function to show modal
+    this.open = function () {
+        let popUpBox = document.getElementById("popUpBox");
+        popUpBox.style.display = "block";
+        document.getElementById("closeModal").innerHTML =
+            '<button onclick="Alert.close()">OK</button>';
+    };
 
-// create functions for function 
-function CustomAlert(){
-
-  // function to show modal
-  this.open = function(){
-    let popUpBox = document.getElementById('popUpBox');
-    popUpBox.style.display = "block";
-    document.getElementById('closeModal').innerHTML = '<button onclick="Alert.close()">OK</button>';
-  }
-  
-  // function to close modal
-  this.close = function(){
-    document.getElementById('popUpBox').style.display = "none";
-    document.getElementById('popUpOverlay').style.display = "none";
-  }
+    // function to close modal
+    this.close = function () {
+        document.getElementById("popUpBox").style.display = "none";
+        document.getElementById("popUpOverlay").style.display = "none";
+    };
 }
