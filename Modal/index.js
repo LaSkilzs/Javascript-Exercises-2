8360e6e380f913020cdffd17e1b51fd281fd1534
Gabriel Lebec <glebec@gmail.com>
2021-03-20 03:04:37 -0400

    feat(modal): remove innerHTML

--- Modal/index.js
+++ Modal/index.js
@@ -1,13 +1,9 @@
 let popUpBox = document.getElementById("popUpBox");
-let popUpOverlay = document.getElementById("popUpOverlay");
 
 function openModal() {
     popUpBox.style.display = "block";
-    document.getElementById("closeModal").innerHTML =
-        '<button onclick="closeModal()">OK</button>';
 }
 
 function closeModal() {
     popUpBox.style.display = "none";
-    popUpOverlay.style.display = "none";
 }
