9ff5567d10f0fa09be7f35a67fb89f86e05220b9
Operations branmanagement <7775577-operations20@users.noreply.gitlab.com>
2021-03-20 10:57:01 -0400

    Add exercise files

---
+++ Carousel/exercise.js
@@ -0,0 +1,18 @@
+// Create a function to showSlide
+function showSlide(slideIndex) {
+   
+}
+
+//  Create a function to show the previous slide
+function previousSlide() {
+    
+}
+
+// Create a function to show the next slide
+function nextSlide() {
+   
+}
+
+
+// Invoke showSlide function
+showSlide();
---
+++ Modal/exercise.js
@@ -0,0 +1,9 @@
+// Create variable for DOM element
+
+
+// Create a funtion to Open Modal
+
+
+// Create a function to Close Modal
+
+
